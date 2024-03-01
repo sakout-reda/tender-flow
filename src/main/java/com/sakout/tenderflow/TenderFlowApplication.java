b9ae52304f7d898bf3e2289eaa3cd6d14fe593bd
sakout-reda <sakout-reda@hotmail.fr>
2024-03-01 13:48:24 +0100

    initial commit

---
+++ src/main/java/com/sakout/tenderflow/TenderFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sakout.tenderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenderFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sakout/tenderflow/TenderFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakout.tenderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
